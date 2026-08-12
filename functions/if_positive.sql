05367fdd0e1195b40b153579cad4448f7b0dd719
pasupuletipranavi@gmail.com <pasupuletipranavi@gmail.com>
2026-08-12 05:07:26 +0000

    functions folder was added and function was implemented

---
+++ functions/if_positive.sql
@@ -0,0 +1,4 @@
+case when 
+  id > 0 
+  then id 
+else 0 end
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,5 +1,6 @@
 select
     id as order_id,
+    -- {{ function('if_positive') }}(id) as order_id_not_null,
     user_id as customer_id,
     order_date,
     status as order_status
